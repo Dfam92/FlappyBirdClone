60d98eea7450b021fcd3c3084534878869f7f30e
Dfam92 <dfam92@hotmail.com>
2021-12-06 19:49:37 -0300

    Procedural track generation completed.
    Was created a for loop to instantiate new tracks in the endPos of previous tracks.

--- FlappyBird Clone/Assets/Runtime/Scripts/Tracks/TrackGenerator.cs
+++ FlappyBird Clone/Assets/Runtime/Scripts/Tracks/TrackGenerator.cs
@@ -7,21 +7,39 @@ public class TrackGenerator : MonoBehaviour
     [SerializeField] private PlayerControl player;
     [SerializeField] private TrackSegment track;
     [SerializeField] private List<TrackSegment> trackList;
-    private List<TrackSegment> nextTrack;
+    [SerializeField] private List<TrackSegment> nextTrack;
+    [SerializeField] private int numberOfSpawnedTracks;
+    private bool newTrack;
 
     private void Start()
     {
         Vector3 startTrackPos = new Vector3(track.EndPos, track.transform.position.y, transform.position.z);
-        
-        if ((player.transform.position.x) > track.StartPos)
+
+        if ((player.transform.position.x) > trackList[trackList.Count - 1].StartPos)
         {
-            Instantiate(track, startTrackPos, Quaternion.identity);
-            nextTrack.Add(track);
+            Instantiate(trackList[trackList.Count - 1], startTrackPos, Quaternion.identity);
+            nextTrack.Add(trackList[trackList.Count - 1]);
+            newTrack = true;
         }
-       
+
     }
     private void Update()
     {
-       
+        if(newTrack)
+        {
+            SpawnTracks();
+            newTrack = false;
+        }
+    }
+
+    private void SpawnTracks()
+    {
+        var newNextTrack = nextTrack[nextTrack.Count - 1];
+        for (int i = 0; i < numberOfSpawnedTracks; i++)
+        {
+            Vector3 previousTrackPos = new Vector3(newNextTrack.EndPos , nextTrack[i].transform.position.y, nextTrack[i].transform.position.z);
+            Instantiate(newNextTrack,previousTrackPos * i, Quaternion.identity);
+            nextTrack.Add(newNextTrack);
+        }
     }
 }
